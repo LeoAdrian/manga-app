e91f1ec70895802b6d1acef2d6ae3f6eddda10d9
Leo-Adrian <apostol.leo.adrian@gmail.com>
2018-06-25 09:20:25 +0300

    send headers for cors

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const app = require('express')();
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-app.all('*', function(req, res, next) {
-  var origin = req.get('origin');
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 app.use(cors());
+
 const connection = mysql.createConnection({
   host     : 'webit-ps.com',
   user     : 'webitpsc_manga',
   password : '12345678',
   database : 'webitpsc_manga'
+});
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use(
@@ -29,28 +36,26 @@ app.use(bodyParser.json());
 app.get('/search', (req, res) => {
   console.log(req.query);
   const { term } = req.query;
-  connection.query(`SELECT title AS t, alias AS a, image_url AS im, hits AS h, status AS s, manga_id AS i FROM allmanga WHERE title LIKE "%${term}%"`, (err, result) => {
-    if(err) throw err;
-    return res.json(result);
-  })
-})
-
-app.get('/manga', (req, res) => {
-  const {id} = req.query;
-  // const filteredQ = `SELECT am.id, am.title, gd.name from allmanga AS am JOIN genre_relation AS mr ON mr.manga_id = am.id JOIN genre_desc as gd ON mr.genre_id = gd.id HAVING gd.name!='ecchi' AND gd.name!='adult' LIMIT ${limit};`
-  connection.query(`SELECT title AS t, alias AS a, image_url AS im, hits AS h, status AS s, manga_id AS i FROM allmanga WHERE i=${id}`, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-    return res.json(result);
+  connection.query(`SELECT title AS t, alias AS a, image_url AS im, hits AS h, status AS s, manga_id AS i FROM allmanga WHERE title LIKE "%${term}%"`, (error, result) => {
+    if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+        return res.json(result);
+      }
   })
 })
 
 app.get('/popular', (req, res) => {
   const {limit} = req.params;
   // const filteredQ = `SELECT am.id, am.title, gd.name from allmanga AS am JOIN genre_relation AS mr ON mr.manga_id = am.id JOIN genre_desc as gd ON mr.genre_id = gd.id HAVING gd.name!='ecchi' AND gd.name!='adult' LIMIT ${limit};`
-  connection.query(`SELECT title AS t, alias AS a, image_url AS im, hits AS h, status AS s, manga_id AS i FROM allmanga ORDER BY hits DESC LIMIT 15`, (err, result) => {
-    if(err) throw err;
-    return res.json(result);
+  connection.query(`SELECT title AS t, alias AS a, image_url AS im, hits AS h, status AS s, manga_id AS i FROM allmanga ORDER BY hits DESC LIMIT 15`, (error, result) => {
+    if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+        return res.json(result);
+      }
   })
 })
 
